a87e38d88da19bd5b6f7e483c1c785178b13a379
vagrant <vagrant@homestead>
2018-03-07 03:41:15 +0000

    blade

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>BookApp</title>
+	@yield('style')
+</head>
+<body>
+		@yield('content')
+</body>
+
+	@yield('script')
+</html>
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,8 @@
+@section('script')
+	<footer>
+		<p>footer</p>
+	</footer>
+	<script type="text/javascript">
+		alert("footer script");
+	</script>
+@endsection
