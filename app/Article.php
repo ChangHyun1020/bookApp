4f7d3b42991c7c48b99604ca1e7b9db83aba3aa4
ChangHyun <ChangHyun1020@github.com>
2018-03-08 05:43:11 +0000

    Eloquent ORM

---
+++ app/Article.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Article extends Model
+{
+	protected $fillable = [
+		'title',
+		'content'
+	];
+
+
+	//relation
+	public function user()
+	{
+		return $this->belongsTo(User::class);
+	}
+
+	public function tags()
+	{
+		return $this->belongsToMany(Tag::class);
+	}
+}
