929f4ac031852428d3861a3df710fc217f763846
ChangH <chyun0000@gmail.com>
2018-03-14 16:34:37 +0900

    다듬질
    Etag를 이용하여 이미지 로딩 속도 개선
    2020-final-touch

--- app/Documentation.php
+++ app/Documentation.php
@@ -7,6 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Documentation
 {
+	public function image($file)
+	{
+		return \Image::make($this->path($file, 'docs/images'));
+	}
+
     public function get($file = 'documentation.md')
     {
     	$content = File::get($this->path($file));
@@ -14,20 +19,24 @@ class Documentation
     	return $this->replaceLinks($content);
     }
 
-    protected function path($file)
+    protected function path($file, $dir = 'docs')
     {
-    	$file = ends_with($file,'.md') ? $file : $file.'.md';
-    	$path = base_path('docs'. DIRECTORY_SEPARATOR . $file);
-
-    	if(! File::exists($path)) {
-    		abort(404, '요정하신 페이지가 없습니다.');
-    	}
-
-    	return $path;
+        $file = ends_with($file, ['.md', '.png']) ? $file : $file . '.md';
+        $path = base_path($dir . DIRECTORY_SEPARATOR . $file);
+        if (! File::exists($path)) {
+            abort(404, '요청하신 페이지를 찾을 수 없습니다.');
+        }
+        return $path;
     }
 
     protected function replaceLinks($content)
     {
     	return str_replace('/docs/{{version}}', '/docs', $content);
+    }
+
+    public function etag($file)
+    {
+    	$lastModified = File::lastModified($this->path($file, 'docs/images'));
+    	return md5($file . $lastModified);
     }
 }
